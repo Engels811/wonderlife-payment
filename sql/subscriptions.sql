ed4fb6cc85aa308ba863c679893eed691637fbd7
Engels811 <54458787+Engels811@users.noreply.github.com>
2025-12-27 03:04:07 +0100

    Delete sql/subscriptions.sql

--- sql/subscriptions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE subscriptions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    discord_id BIGINT NOT NULL,
-    product VARCHAR(100),
-    role_id BIGINT,
-    start_date DATETIME,
-    end_date DATETIME,
-    active BOOLEAN DEFAULT TRUE
-);
