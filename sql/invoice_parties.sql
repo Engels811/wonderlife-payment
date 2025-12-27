80f198eae8072a09694d4cb985f9a867ee784815
Engels811 <54458787+Engels811@users.noreply.github.com>
2025-12-27 03:03:41 +0100

    Delete sql/invoice_parties.sql

--- sql/invoice_parties.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE payments
-ADD COLUMN customer_name VARCHAR(100),
-ADD COLUMN customer_email VARCHAR(150),
-ADD COLUMN customer_address TEXT,
-ADD COLUMN company_name VARCHAR(150),
-ADD COLUMN vat_id VARCHAR(50);
