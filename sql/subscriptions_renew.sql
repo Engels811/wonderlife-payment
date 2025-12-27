75de86d58ec7fccb2d5a342e282420ce59da49b0
Engels811 <54458787+Engels811@users.noreply.github.com>
2025-12-27 03:04:16 +0100

    Delete sql/subscriptions_renew.sql

--- sql/subscriptions_renew.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE subscriptions
-ADD COLUMN auto_renew BOOLEAN DEFAULT FALSE,
-ADD COLUMN last_renewed DATETIME,
-ADD COLUMN payment_provider VARCHAR(20),
-ADD COLUMN price_cents INT,
-ADD COLUMN billing_interval_days INT;
